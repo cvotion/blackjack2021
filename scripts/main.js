5d6924e836ef2f64b215f28dbbb50a967f30d1d9
Christian Votion <christianvotion@gmail.com>
2022-08-02 09:10:14 -0500

    added betting feature and made sdome code organization

--- scripts/main.js
+++ scripts/main.js
@@ -1,20 +1,31 @@
-
+//! ////////// Getting player/dealer hands and buttons//////////
 let player= document.querySelector('#player-hand');
-let dealer = document.querySelector('#dealer-hand')
+let dealer = document.querySelector('#dealer-hand');
 let button = document.querySelector('.buttons');
+//! ////////// Creating Hidden Card Image //////////
 let hiddenImage = document.createElement('img')
 hiddenImage.setAttribute('src', `images/NicePng_playing-card-back-png_1215756.png`)
-let dealt = false
-
+let bet = document.querySelector('.bet');
+//! ////////// Creating Deck and Player Arrays //////////
 let suits = ['spades', 'hearts', 'clubs', 'diamonds']
 let ranks = [2, 3, 4, 5, 6, 7, 8, 9, 10, 'jack', 'queen', 'king', 'ace']
 let deck = []
 let playerHand = []
 let dealerHand = []
+//! ////////// Creating Deck Arrays //////////
+let dealt = false
+let betAmount = 0
 let playerSum = 0
 let dealerSum = 0
+createDeck()
+let shuffledDeck = shuffleArray(deck);
+let hiddenCard = shuffledDeck.pop()
+let playerMoney = 20
+let message = document.querySelector('.message-container');
+let playerScore = document.querySelector('.player-name')
+playerScore.innerText = `Player: $ ${playerMoney}`
 
-//!Shuffle
+//! //////////////// Shuffle ////////////////
 function shuffleArray(array) {
     for (var i = array.length - 1; i > 0; i--) {
         var j = Math.floor(Math.random() * (i + 1));
@@ -24,37 +35,38 @@ function shuffleArray(array) {
     }
     return array;
 }
-//!Create Deck
-for (let i = 0; i < suits.length; i++){
-    for (let j =0 ; j < ranks.length; j++){
-        if(j < 9){
-            deck.push({
-                'suit': `${suits[i]}`,
-                'rank': `${ranks[j]}`,
-                'img' : `images/${ranks[j]}_of_${suits[i]}.png`,
-                'points': j+2
-            })
+//! //////////////// Create Deck ////////////////
+function createDeck(){
+    for (let i = 0; i < suits.length; i++){
+        for (let j =0 ; j < ranks.length; j++){
+            if(j < 9){
+                deck.push({
+                    'suit': `${suits[i]}`,
+                    'rank': `${ranks[j]}`,
+                    'img' : `images/${ranks[j]}_of_${suits[i]}.png`,
+                    'points': j+2
+                })
+            }
+            else if (j === 12){
+                deck.push({
+                    'suit': `${suits[i]}`,
+                    'rank': `${ranks[j]}`,
+                    'img' : `images/${ranks[j]}_of_${suits[i]}.png`,
+                    'points': 11
+                })
+            }
+            else {
+                deck.push({
+                    'suit': `${suits[i]}`,
+                    'rank': `${ranks[j]}`,
+                    'img' : `images/${ranks[j]}_of_${suits[i]}.png`,
+                    'points': 10
+                })
+            }
         }
-        else if (j === 12){
-            deck.push({
-                'suit': `${suits[i]}`,
-                'rank': `${ranks[j]}`,
-                'img' : `images/${ranks[j]}_of_${suits[i]}.png`,
-                'points': 11
-            })
-        }
-        else {
-            deck.push({
-                'suit': `${suits[i]}`,
-                'rank': `${ranks[j]}`,
-                'img' : `images/${ranks[j]}_of_${suits[i]}.png`,
-                'points': 10
-            })
-        }
+        console.log(deck);
     }
-    console.log(deck);
 }
-let shuffledDeck = shuffleArray(deck);
 function dealCard(p){
     let pCard = shuffledDeck.pop()
     pHand.push(pCard)
@@ -62,15 +74,14 @@ function dealCard(p){
     newimage1.setAttribute('src', `${pCard['img']}`)
     player.append(newimage1)
 }
-//!Deal
-let hiddenCard = shuffledDeck.pop()
+//! //////////////// Deal ////////////////
 function deal(){
     dealerHand.push(hiddenCard)
     let dealerCard = shuffledDeck.pop()
     dealerHand.push(dealerCard)
     dealer.append(hiddenImage)
     let newimage2 = document.createElement('img')
-    newimage2.setAttribute('src', `${dealerCard['img']}`)
+    newimage2.setAttribute('src', `${dealerCard.img}`)
     dealer.append(newimage2)
     for (let i = 0; i <2; i ++){
         let playerCard = shuffledDeck.pop()
@@ -80,7 +91,7 @@ function deal(){
         player.append(newimage1)
     }
 }
-//!Hit
+//! //////////////// Hit ////////////////
 function hit(){
     let playerCard = shuffledDeck.pop()
     playerHand.push(playerCard)
@@ -89,7 +100,7 @@ function hit(){
     player.append(newimage1)
     
 }
-//!Get Card Values
+//! //////////////// Get Card Values ////////////////
 function getCardValues(player){
     let sum = 0
     for (i = 0; i < player.length; i ++){
@@ -99,11 +110,45 @@ function getCardValues(player){
     console.log(sum);
     return sum
 }
-function over21(){
-    if (playerSum > 21){
-        console.log("you lose");
+//! //////////////// Check for Ace ////////////////
+function playerAce(){
+    for (let i = 0; i < playerHand.length; i ++){
+        if (playerHand[i].rank === 'ace' && playerSum > 21){
+            playerHand[i].points = 1
+        }
     }
 }
+function dealerAce(){
+    for (let i = 0; i < dealerHand.length; i ++){
+        if (dealerHand[i].rank === 'ace' && dealerSum > 21){
+            dealerHand[i].points = 1
+            return true
+        }
+    }
+}
+//! //////////////// Message ////////////////
+function youWin(){
+    let message = document.querySelector('.message-container');
+    let newMessage = document.createElement('div')
+    newMessage.setAttribute('class', 'message green')
+    newMessage.innerText = 'You Win!'
+    message.append(newMessage)
+    playerMoney += betAmount * 1.5
+}
+function youLose(){
+    let message = document.querySelector('.message-container');
+    let newMessage = document.createElement('div')
+    newMessage.setAttribute('class', 'message red')
+    newMessage.innerText = 'You Lose!'
+    message.append(newMessage)
+    playerMoney -= betAmount
+}
+function over21(){
+    if (playerSum > 21){
+        youLose()
+    }
+}
+//! //////////////// Buttons ////////////////
 button.addEventListener('click', (e)=>{
     if (e.target.innerText === 'Deal'){
         if (dealt === false){
@@ -115,38 +160,81 @@ button.addEventListener('click', (e)=>{
         }
     }
     else if (e.target.innerText === 'Hit'){
-        hit()
-        dealerSum = getCardValues(dealerHand)
-        playerSum = getCardValues(playerHand)
-        over21()
+        if (dealt === true){    
+            hit()
+            playerSum = getCardValues(playerHand)
+            playerAce()
+            playerSum = getCardValues(playerHand)
+            over21()
+        }
     }
     else if (e.target.innerText === 'Stand'){
-        hiddenImage.setAttribute('src', `${hiddenCard['img']}`)
-        dealerSum = getCardValues(dealerHand)
-        playerSum = getCardValues(playerHand)
-        if (dealerSum < 17){
-            let dealerCard = shuffledDeck.pop()
-            dealerHand.push(dealerCard)
-            let newimage2 = document.createElement('img')
-            newimage2.setAttribute('src', `${dealerCard['img']}`)
-            dealer.append(newimage2)
+        console.log('click');
+        if (dealt === true){
+            hiddenImage.setAttribute('src', `${hiddenCard['img']}`)
+            dealerSum = getCardValues(dealerHand)
+            playerSum = getCardValues(playerHand)
+            if (dealerSum < 17){
+                let dealerCard = shuffledDeck.pop()
+                dealerHand.push(dealerCard)
+                let newimage2 = document.createElement('img')
+                newimage2.setAttribute('src', `${dealerCard['img']}`)
+                dealer.append(newimage2)
+            }
+            dealerSum = getCardValues(dealerHand)
+            dealerAce()
+            dealerSum = getCardValues(dealerHand)
+            if (playerSum === 21){
+                youWin()
+            }
+            else if (playerSum < 21 && playerSum > dealerSum){
+                youWin()
+            }
+            else if (dealerSum > 21){
+                youWin()
+            }
+            else if(playerSum > 21){
+                youLose()
+            }
+            else if (dealerSum <= 21 && dealerSum > playerSum){
+                youLose()
+            }
+            else if (dealerSum === playerSum){
+                let newMessage = document.createElement('div')
+                newMessage.setAttribute('class', 'message yellow')
+                newMessage.innerText = 'Tie!'
+                message.append(newMessage)
+                
+            }
         }
-        dealerSum = getCardValues(dealerHand)
-        if (playerSum === 21){
-            console.log("you win");
-        }
-        else if (playerSum < 21 && playerSum > dealerSum){
-            console.log("you win");
-        }
-        else if (dealerSum > 21){
-            console.log('you win');
-        }
-        else if(playerSum > 21){
-            console.log('you lose');
-        }
-        else if (dealerSum <= 21 && dealerSum > playerSum){
-            console.log('you lose');
-        }
-        
+    }    
+    else if (e.target.innerText === '+'){
+        console.log('clicked');
+        betAmount += 1
+        bet.innerText = `$ ${betAmount}`
+    }
+    else if (e.target.innerText === '-' && betAmount > 1){
+        console.log('clicked');
+        betAmount -= 1
+        bet.innerText = `$ ${betAmount}`
+    }
+    else if (e.target.innerText === 'Reset'){
+        console.log('reset');
+        hiddenImage.setAttribute('src', `images/NicePng_playing-card-back-png_1215756.png`)
+        dealt = false
+        betAmount = 0
+        deck = []
+        playerHand = []
+        dealerHand = []
+        playerSum = 0
+        dealerSum = 0
+        createDeck()
+        shuffledDeck = shuffleArray(deck);
+        hiddenCard = shuffledDeck.pop()
+        player.innerHTML = ''
+        dealer.innerHTML = ''
+        message.innerHTML = ''
+        playerScore.innerText = `Player: $ ${playerMoney}`
+        bet.innerText = `$ ${betAmount}`
     }
 })
